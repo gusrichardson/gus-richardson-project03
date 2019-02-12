b8284ef27abd6bc28a73ed3b2020dcdb064a7130
Gus <gusrichardson1@gmail.com>
2019-02-12 15:56:39 -0500

    first commit

---
+++ js/jQuery.js
@@ -0,0 +1,42 @@
+const failures = {
+    babies: {
+        painful: [
+            {
+                title: "slackline",
+                intensity: 1,
+                gif: <iframe src="https://giphy.com/embed/f9RIpuEitaLuiczwFs" width="384" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe> <p><a href="https://giphy.com/gifs/monday-mondays-thud-f9RIpuEitaLuiczwFs">via GIPHY</a></p>
+            },
+            {
+                title: "slide fail",
+                intensity: 1,
+            }
+        ],
+        embarrassing: [
+
+        ]
+    },
+    nonBabies: {
+        painful: [
+
+        ],
+        embarrassing: [
+
+        ]
+    },
+    sports: {
+        painful: [
+
+        ],
+        embarrassing: [
+
+        ]
+    },
+    animals: {
+        painful: [
+
+        ],
+        embarrassing: [
+
+        ]
+    }
+};
