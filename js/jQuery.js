c17ef15781d130719886223706ac56dc1075b8c5
Gus <gusrichardson1@gmail.com>
2019-02-12 16:04:07 -0500

    commit 2, updated Failures object

--- js/jQuery.js
+++ js/jQuery.js
@@ -4,14 +4,46 @@ const failures = {
             {
                 title: "slackline",
                 intensity: 1,
-                gif: <iframe src="https://giphy.com/embed/f9RIpuEitaLuiczwFs" width="384" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe> <p><a href="https://giphy.com/gifs/monday-mondays-thud-f9RIpuEitaLuiczwFs">via GIPHY</a></p>
+                gif: '<iframe src="https://giphy.com/embed/f9RIpuEitaLuiczwFs" width="384" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/monday-mondays-thud-f9RIpuEitaLuiczwFs">via GIPHY</a></p>'
             },
             {
                 title: "slide fail",
                 intensity: 1,
-            }
+                gif: '<iframe src="https://giphy.com/embed/BhbUcnKQvQiwE" width="480" height="362" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/fail-lol-slide-BhbUcnKQvQiwE">via GIPHY</a></p>'
+            },
+            {
+                title: "huskie slam",
+                intensity: 2,
+                gif: ' <iframe src="https://giphy.com/embed/F09NFq9b23Xpu" width="480" height="290" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/dog-fall-funny-gif-F09NFq9b23Xpu">via GIPHY</a></p>'
+            },
+            {
+                title: "twerky mum bum",
+                intensity: 2,
+                gif: '<iframe src="https://giphy.com/embed/h1ojsprlDCgbS" width="301" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/dancing-lol-mom-h1ojsprlDCgbS">via GIPHY</a></p>'
+            },
         ],
         embarrassing: [
+            {
+                title: "brain freeze",
+                intensity: 1,
+                gif: '<iframe src="https://giphy.com/embed/3uFIPLaVK6iFW" width="270" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/funniest-funniest8mandatorygifsoftheweek71317-3uFIPLaVK6iFW">via GIPHY</a></p>'
+
+            },
+            {
+                title: "hoser",
+                intensity: 1,
+                gif: '<iframe src="https://giphy.com/embed/iwKvMdNQomKC4" width="480" height="320" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/afv-fail-babies-iwKvMdNQomKC4">via GIPHY</a></p>'
+            },
+            {
+                title: "snot baby",
+                intensity: 2,
+                gif: '<iframe src="https://giphy.com/embed/AxVvjIqj8aWBzF85jy" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/worldsfunniestfails-fails-funniest-worlds-AxVvjIqj8aWBzF85jy">via GIPHY</a></p>'
+            },
+            {
+                title: "spaghetti",
+                intensity: 2,
+                gif: '<iframe src="https://giphy.com/embed/xTk9ZP6AM0EKyQwqNW" width="480" height="382" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/afv-funny-fail-lol-xTk9ZP6AM0EKyQwqNW">via GIPHY</a></p>'
+            }
 
         ]
     },
