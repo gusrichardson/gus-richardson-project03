f9dc1d741ebccb1a0b010572f5bcad3e09b242ec
Gus <gusrichardson1@gmail.com>
2019-02-12 16:20:57 -0500

    finished nonbabies array

--- js/jQuery.js
+++ js/jQuery.js
@@ -2,45 +2,45 @@ const failures = {
     babies: {
         painful: [
             {
-                title: "slackline",
+                title: 'slackline',
                 intensity: 1,
                 gif: '<iframe src="https://giphy.com/embed/f9RIpuEitaLuiczwFs" width="384" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/monday-mondays-thud-f9RIpuEitaLuiczwFs">via GIPHY</a></p>'
             },
             {
-                title: "slide fail",
+                title: 'slide fail',
                 intensity: 1,
                 gif: '<iframe src="https://giphy.com/embed/BhbUcnKQvQiwE" width="480" height="362" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/fail-lol-slide-BhbUcnKQvQiwE">via GIPHY</a></p>'
             },
             {
-                title: "huskie slam",
+                title: 'huskie slam',
                 intensity: 2,
                 gif: ' <iframe src="https://giphy.com/embed/F09NFq9b23Xpu" width="480" height="290" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/dog-fall-funny-gif-F09NFq9b23Xpu">via GIPHY</a></p>'
             },
             {
-                title: "twerky mum bum",
+                title: 'twerky mum bum',
                 intensity: 2,
                 gif: '<iframe src="https://giphy.com/embed/h1ojsprlDCgbS" width="301" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/dancing-lol-mom-h1ojsprlDCgbS">via GIPHY</a></p>'
             },
         ],
         embarrassing: [
             {
-                title: "brain freeze",
+                title: 'brain freeze',
                 intensity: 1,
                 gif: '<iframe src="https://giphy.com/embed/3uFIPLaVK6iFW" width="270" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/funniest-funniest8mandatorygifsoftheweek71317-3uFIPLaVK6iFW">via GIPHY</a></p>'
 
             },
             {
-                title: "hoser",
+                title: 'hoser',
                 intensity: 1,
                 gif: '<iframe src="https://giphy.com/embed/iwKvMdNQomKC4" width="480" height="320" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/afv-fail-babies-iwKvMdNQomKC4">via GIPHY</a></p>'
             },
             {
-                title: "snot baby",
+                title: 'snot baby',
                 intensity: 2,
                 gif: '<iframe src="https://giphy.com/embed/AxVvjIqj8aWBzF85jy" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/worldsfunniestfails-fails-funniest-worlds-AxVvjIqj8aWBzF85jy">via GIPHY</a></p>'
             },
             {
-                title: "spaghetti",
+                title: 'spaghetti',
                 intensity: 2,
                 gif: '<iframe src="https://giphy.com/embed/xTk9ZP6AM0EKyQwqNW" width="480" height="382" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/afv-funny-fail-lol-xTk9ZP6AM0EKyQwqNW">via GIPHY</a></p>'
             }
@@ -50,31 +50,48 @@ const failures = {
     nonBabies: {
         painful: [
             {
-                title: "yoyo fail",
+                title: 'yoyo fail',
                 intensity: 1,
                 gif: '<iframe src="https://giphy.com/embed/KYXWGxC3gcCpq" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/fail-pain-yoyo-KYXWGxC3gcCpq">via GIPHY</a></p>'
             },
             {
-                title: "shameful display",
+                title: 'shameful display',
                 intensity: 1,
                 gif: '<iframe src="https://giphy.com/embed/NQL7Wuo2JSQSY" width="283" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/fail-fall-jump-NQL7Wuo2JSQSY">via GIPHY</a></p>'
             },
             {
-                title: "watermelon juice",
+                title: 'watermelon juice',
                 intensity: 2,
                 gif: '<iframe src="https://giphy.com/embed/RkHYWBm3VnfX2" width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/ouch-fails-watermelon-RkHYWBm3VnfX2">via GIPHY</a></p>'
             },
             {
-                title: "no pain no gain",
+                title: 'no pain no gain',
                 intensity: 2,
                 gif: '<iframe src="https://giphy.com/embed/DuWJ9i10UXtf2" width="480" height="321" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/fail-ouch-pain-DuWJ9i10UXtf2">via GIPHY</a></p>'
             }
         ],
         embarrassing: [
             {
+                title: 'bad posture',
+                intensity: 1,
+                gif: '<iframe src="https://giphy.com/embed/FewMZauWeGB0IpNpVO" width="384" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/reaction-mood-FewMZauWeGB0IpNpVO">via GIPHY</a></p>'
 
+            },
+            {
+                title: 'sit down',
+                intensity: 1,
+                gif: '<iframe src="https://giphy.com/embed/l1tmxuXJgtlaaEqwwo" width="479" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/fallontonight-lol-omg-l1tmxuXJgtlaaEqwwo">via GIPHY</a></p>'
+            },
+            {
+                title: 'beer explosion',
+                intensity: 2,
+                gif: '<iframe src="https://giphy.com/embed/ABoaCMjkoqmIg" width="270" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/ABoaCMjkoqmIg">via GIPHY</a></p>'
+            },
+            {
+                title: 'sweet moves',
+                intensity: 2,
+                gif: '<iframe src="https://giphy.com/embed/IYte12qWWgCt2" width="291" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/IYte12qWWgCt2">via GIPHY</a></p>'
             }
-
         ]
     },
     sports: {
